230904ba9380dc06efd8120cdf5003ea91c5a3fc
SaiMajesty <mallikarjunasai174@gmail.com>
2023-01-28 02:33:13 -1200

    Ku Entertainments

--- src/components/Home/MarketingSolutions/MarketingSolutions.jsx
+++ src/components/Home/MarketingSolutions/MarketingSolutions.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import box1 from "../../../assets/marketing/box-1.png";
 import box2 from "../../../assets/marketing/box-2.png";
 import box3 from "../../../assets/marketing/box-3.png";
+
 import box4 from "../../../assets/marketing/box-4.png";
 
 const MarketingSolutions = () => {
@@ -18,8 +19,8 @@ const MarketingSolutions = () => {
           </p>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-12 my-20 gap-20">
-          <div className="col-span-8">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+          <div className="col-span-4 sm:col-span-4 md:col-span-8">
+            <div className="grid grid-cols-1  md:grid-cols-2  gap-10">
               <div className="bg-white p-10 rounded-lg">
                 <h4 className="text-2xl my-5 text-secondary font-semibold">
                   Email Marketing
@@ -31,12 +32,12 @@ Our services include,
 
 Email list building, We will help you develop a targeted email list of potential customers and fans.
                 </p>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
-                  <button className="bg-secondary h-1/3 rounded text-xl text-white">
+               
+                  <button className="bg-secondary py-3 px-5 rounded text-xl text-white">
                     Learn More
                   </button>
-                  <img className="" src={box1} alt="" />
-                </div>
+                  <img className="w-52" src={box1} alt="" />
+            
               </div>
               <div className="bg-secondary p-10 rounded-lg">
                 <h4 className="text-2xl my-5 text-primary font-semibold">
@@ -45,27 +46,27 @@ Email list building, We will help you develop a targeted email list of potential
                 <p className="my-5 text-white">
                 Ku entertainment has had great success in stratergy Developement. Through the implementation of Startergy, a company-wide strategery, they have been able to gain competitive advantage over their competitors. The company regularly solicits suggestions from employees on ways to further develop and refine their strategery for even greater success.
                 </p>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
-                  <button className="bg-white h-1/3 rounded text-xl">
+                
+                  <button className="bg-white  rounded text-xl py-3 px-5">
                     Learn More
                   </button>
-                  <img className="" src={box2} alt="" />
-                </div>
+                 
+                  <img className="w-52" src={box2} alt="" />
               </div>
             </div>
-            <div className="bg-secondary p-10 rounded-lg mt-10">
+            <div className="col-span-4 bg-secondary p-10 rounded-lg mt-10">
               <h4 className="text-2xl my-5 text-primary font-semibold">
                 Content Stratergy
               </h4>
               <p className="my-5 text-white">
               Ku entertainment offers a wide range of web content design and development services. They understand the importance of having a solid content stratergy and are committed to creating engaging, relevant and impactful content for their clients. They keep up with the latest trends in the industry to ensure that their content is as effective as possible.
               </p>
-              <div className="flex gap-10 items-center justify-between">
-                <button className="bg-white h-1/3 rounded text-xl py-3 px-5">
+              
+                <button className="bg-white text-secondary rounded text-xl py-3 px-5">
                   Learn More
                 </button>
-                <img className="w-1/6" src={box3} alt="" />
-              </div>
+                <img className="w-36" src={box3} alt="" />
+             
             </div>
           </div>
           <div className="col-span-4 bg-white p-10 rounded-lg ">
